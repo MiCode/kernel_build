f5efcad705c9da0adaae73ed2266d798d22a7333
Jeevan Shriram <jshriram@codeaurora.org>
2018-12-07 13:49:35 -0800

    build scripts: Add initial version of build scripts
    Add initial version of build scripts for compiling kernel and generate
    prebuilt binaries in <ROOT DIR>/kernel/ship_prebuilt.
    
    Change-Id: I166addd73af2f54875f33755c8563aa73f560642

---
+++ buildkernel.sh
@@ -0,0 +1,165 @@
+#!/bin/bash -xE
+
+# Usage:
+#   build/build.sh <make options>*
+#
+# The kernel is built in ${COMMON_OUT_DIR}/${KERNEL_DIR}.
+#
+# TODO: External module compilation
+
+set -e
+
+export ROOT_DIR=$(readlink -f $(dirname $0)/../../..)
+export MAKE_ARGS=$@
+export COMMON_OUT_DIR=$(readlink -m ${OUT_DIR:-${ROOT_DIR}/out/${BRANCH}})
+export OUT_DIR=$(readlink -m ${COMMON_OUT_DIR})
+export MODULES_STAGING_DIR=$(readlink -m ${COMMON_OUT_DIR}/staging)
+export KERNEL_PREBUILT_DIR=$(readlink -m ${KERNEL_DIR}/../ship_prebuilt)
+export MODULES_PRIVATE_DIR=$(readlink -m ${COMMON_OUT_DIR}/private)
+export DIST_DIR=$(readlink -m ${DIST_DIR:-${COMMON_OUT_DIR}/dist})
+export UNSTRIPPED_DIR=${DIST_DIR}/unstripped
+export CLANG_TRIPLE CROSS_COMPILE CROSS_COMPILE_ARM32 ARCH SUBARCH
+
+# Setting up for build
+PREBUILT_KERNEL_IMAGE=$(basename ${TARGET_PREBUILT_INT_KERNEL})
+IMAGE_FILE_PATH=arch/${ARCH}/boot
+FILES="
+vmlinux
+System.map
+"
+
+cd ${ROOT_DIR}
+
+if [ ! -e ${OUT_DIR} ]; then
+	mkdir -p ${OUT_DIR}
+fi
+
+if [ -z "${FORCE_KERNEL_BUILD}" ]; then
+	if [ -d ${KERNEL_PREBUILT_DIR} ]; then
+		echo "Kernel Prebuilt directory exist, copying binaries..."
+		# Copy headers
+		mkdir -p ${KERNEL_HEADERS_INSTALL}
+		cd ${KERNEL_PREBUILT_DIR}/usr;find  -name *.h -exec cp --parents {} ${ROOT_DIR}/${KERNEL_HEADERS_INSTALL} \;
+		cd ${ROOT_DIR}
+
+		# Copy Image, vmlinux, System.map
+		cd ${KERNEL_PREBUILT_DIR}
+		for FILE in ${FILES}; do
+			if [ -f ${KERNEL_PREBUILT_DIR}/$FILE ]; then
+				# Copy for prebuilt
+				echo "  $FILE ${KERNEL_PREBUILT_DIR}"
+				echo ${KERNEL_PREBUILT_DIR}/${FILE}
+				cp -p ${KERNEL_PREBUILT_DIR}/${FILE} ${OUT_DIR}/
+				echo $FILE copied to ${KERNEL_PREBUILT_DIR}
+			else
+				echo "$FILE does not exist, skipping"
+			fi
+		done
+		if [ ! -e ${OUT_DIR}/${IMAGE_FILE_PATH} ]; then
+			mkdir -p ${OUT_DIR}/${IMAGE_FILE_PATH}
+		fi
+		cp -p ${KERNEL_PREBUILT_DIR}/${IMAGE_FILE_PATH}/${PREBUILT_KERNEL_IMAGE} ${OUT_DIR}/${IMAGE_FILE_PATH}/${PREBUILT_KERNEL_IMAGE}
+
+		cd ${ROOT_DIR}
+		# Copy Modules
+		MODULES=$(find ${KERNEL_PREBUILT_DIR} -type f -name "*.ko")
+		if [ ! -e  ${KERNEL_MODULES_OUT} ]; then
+			mkdir -p  ${KERNEL_MODULES_OUT}
+		fi
+		for FILE in ${MODULES}; do
+			echo "Copy ${FILE#${KERNEL_MODULES_OUT}/}"
+			cp -p ${FILE} ${KERNEL_MODULES_OUT}
+		done
+
+		#return success
+		exit 0
+	else
+		echo "Kernel Prebuilt directory doesn't exist, compiling..."
+	fi
+fi
+
+
+if [ -z "${SKIP_DEFCONFIG}" ] ; then
+  set -x
+  (cd ${KERNEL_DIR} && make O=${OUT_DIR} ${DEFCONFIG})
+  set +x
+fi
+
+if [ -n "${CC}" ]; then
+  CC_ARG="CC=${CC}"
+fi
+
+#Install headers
+set -x
+  (cd ${OUT_DIR} && make O=${OUT_DIR} ${CC_ARG} ${MAKE_ARGS} headers_install)
+set +x
+
+
+# Building Kernel
+set -x
+  (cd ${OUT_DIR} && make O=${OUT_DIR} ${CC_ARG} -j($(nproc)/2) ${MAKE_ARGS})
+set +x
+
+# Modules Install
+rm -rf ${MODULES_STAGING_DIR}
+mkdir -p ${MODULES_STAGING_DIR}
+set -x
+(cd ${OUT_DIR} && \
+   make O=${OUT_DIR} ${CC_ARG} INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=${MODULES_STAGING_DIR} ${MAKE_ARGS} modules_install)
+set +x
+
+echo ${KERNEL_PREBUILT_DIR}
+
+if [[ ! -e ${KERNEL_PREBUILT_DIR} ]]; then
+	mkdir -p ${KERNEL_PREBUILT_DIR}
+fi
+
+if [[ ! -e ${KERNEL_MODULES_OUT} ]]; then
+	mkdir -p ${KERNEL_MODULES_OUT}
+fi
+
+MODULES=$(find ${MODULES_STAGING_DIR} -type f -name "*.ko")
+if [ -n "${MODULES}" ]; then
+  echo "======================"
+  echo " Copying modules files"
+    for FILE in ${MODULES}; do
+      echo "${FILE#${KERNEL_MODULES_OUT}/}"
+      cp -p ${FILE} ${KERNEL_MODULES_OUT}
+
+      # Copy for prebuilt
+      if [ ! -e ${KERNEL_PREBUILT_DIR}/${KERNEL_MODULES_OUT} ]; then
+	      mkdir -p ${KERNEL_PREBUILT_DIR}/${KERNEL_MODULES_OUT}
+      fi
+      cp -p ${FILE} ${KERNEL_PREBUILT_DIR}/${KERNEL_MODULES_OUT}
+
+    done
+fi
+
+echo "============="
+echo "Copying files"
+for FILE in ${FILES}; do
+  if [ -f ${OUT_DIR}/${FILE} ]; then
+    # Copy for prebuilt
+    echo "  $FILE ${KERNEL_PREBUILT_DIR}"
+    cp -p ${OUT_DIR}/${FILE} ${KERNEL_PREBUILT_DIR}/
+    echo $FILE copied to ${KERNEL_PREBUILT_DIR}
+  else
+    echo "  $FILE does not exist, skipping"
+  fi
+done
+
+
+if [ ! -e ${KERNEL_PREBUILT_DIR}/${IMAGE_FILE_PATH} ]; then
+	mkdir -p ${KERNEL_PREBUILT_DIR}/${IMAGE_FILE_PATH}
+fi
+
+cp -p ${OUT_DIR}/${IMAGE_FILE_PATH}/${PREBUILT_KERNEL_IMAGE} ${KERNEL_PREBUILT_DIR}/${IMAGE_FILE_PATH}/${PREBUILT_KERNEL_IMAGE}
+
+CUR_DIR=$(pwd)
+echo "============"
+echo "Copy headers"
+mkdir -p ${KERNEL_PREBUILT_DIR}/usr
+cd ${KERNEL_HEADERS_INSTALL};find  -name *.h -exec cp --parents {} ${KERNEL_PREBUILT_DIR}/usr \;
+cd $CUR_DIR
+
+exit 0
---
+++ kernel_definitions.mk
@@ -0,0 +1,106 @@
+# Android Kernel common definitions
+
+
+KERNEL_TARGET := $(strip $(INSTALLED_KERNEL_TARGET))
+ifeq ($(KERNEL_TARGET),)
+INSTALLED_KERNEL_TARGET := $(PRODUCT_OUT)/kernel
+endif
+
+TARGET_KERNEL_MAKE_ENV := $(strip $(TARGET_KERNEL_MAKE_ENV))
+ifeq ($(TARGET_KERNEL_MAKE_ENV),)
+KERNEL_MAKE_ENV :=
+else
+KERNEL_MAKE_ENV := $(TARGET_KERNEL_MAKE_ENV)
+endif
+
+TARGET_KERNEL_ARCH := $(strip $(TARGET_KERNEL_ARCH))
+ifeq ($(TARGET_KERNEL_ARCH),)
+KERNEL_ARCH := arm
+else
+KERNEL_ARCH := $(TARGET_KERNEL_ARCH)
+endif
+
+ifeq ($(shell echo $(KERNEL_DEFCONFIG) | grep vendor),)
+KERNEL_DEFCONFIG := vendor/$(KERNEL_DEFCONFIG)
+endif
+
+# Force 32-bit binder IPC for 64bit kernel with 32bit userspace
+ifeq ($(KERNEL_ARCH),arm64)
+ifeq ($(TARGET_ARCH),arm)
+KERNEL_CONFIG_OVERRIDE := CONFIG_ANDROID_BINDER_IPC_32BIT=y
+endif
+endif
+
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(strip $(TARGET_KERNEL_CROSS_COMPILE_PREFIX))
+ifeq ($(TARGET_KERNEL_CROSS_COMPILE_PREFIX),)
+KERNEL_CROSS_COMPILE := arm-eabi-
+else
+KERNEL_CROSS_COMPILE := $(shell pwd)/$(TARGET_TOOLS_PREFIX)
+endif
+
+ifeq ($(KERNEL_LLVM_SUPPORT), true)
+  ifeq ($(KERNEL_SD_LLVM_SUPPORT), true)  #Using sd-llvm compiler
+    ifeq ($(shell echo $(SDCLANG_PATH) | head -c 1),/)
+       KERNEL_LLVM_BIN := $(SDCLANG_PATH)/clang
+    else
+       KERNEL_LLVM_BIN := $(shell pwd)/$(SDCLANG_PATH)/clang
+    endif
+    $(warning "Using sdllvm" $(KERNEL_LLVM_BIN))
+  else
+     KERNEL_LLVM_BIN := $(shell pwd)/$(CLANG) #Using aosp-llvm compiler
+    $(warning "Using aosp-llvm" $(KERNEL_LLVM_BIN))
+  endif
+endif
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+
+KERNEL_GCC_NOANDROID_CHK := $(shell (echo "int main() {return 0;}" | $(KERNEL_CROSS_COMPILE)gcc -E -mno-android - > /dev/null 2>&1 ; echo $$?))
+
+real_cc :=
+ifeq ($(KERNEL_LLVM_SUPPORT),true)
+real_cc := REAL_CC=$(KERNEL_LLVM_BIN) CLANG_TRIPLE=aarch64-linux-gnu-
+else
+ifeq ($(strip $(KERNEL_GCC_NOANDROID_CHK)),0)
+KERNEL_CFLAGS := KCFLAGS=-mno-android
+endif
+endif
+
+TARGET_KERNEL := msm-$(TARGET_KERNEL_VERSION)
+BUILD_ROOT_LOC := ../../..
+TARGET_KERNEL_SOURCE := kernel/$(TARGET_KERNEL)
+KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/kernel/$(TARGET_KERNEL)
+KERNEL_SYMLINK := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
+KERNEL_USR := $(KERNEL_SYMLINK)/usr
+
+KERNEL_CONFIG := $(KERNEL_OUT)/.config
+
+ifeq ($(KERNEL_DEFCONFIG)$(wildcard $(KERNEL_CONFIG)),)
+$(error Kernel configuration not defined, cannot build kernel)
+else
+
+ifeq ($(TARGET_USES_UNCOMPRESSED_KERNEL),true)
+$(info Using uncompressed kernel)
+TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/Image
+else
+ifeq ($(KERNEL_ARCH),arm64)
+TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/Image.gz
+else
+TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/zImage
+endif
+endif
+
+ifeq ($(TARGET_KERNEL_APPEND_DTB), true)
+$(info Using appended DTB)
+TARGET_PREBUILT_INT_KERNEL := $(TARGET_PREBUILT_INT_KERNEL)-dtb
+endif
+
+KERNEL_HEADERS_INSTALL := $(KERNEL_OUT)/usr
+KERNEL_MODULES_INSTALL ?= system
+KERNEL_MODULES_OUT ?= $(PRODUCT_OUT)/$(KERNEL_MODULES_INSTALL)/lib/modules
+
+TARGET_PREBUILT_KERNEL := $(TARGET_PREBUILT_INT_KERNEL)
+
+BOARD_VENDOR_KERNEL_MODULES += $(shell ls $(KERNEL_MODULES_OUT)/*.ko)
+
+endif
+endif
