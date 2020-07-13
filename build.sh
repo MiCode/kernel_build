0d1ae60815ff3c4821f8ac38480f11b65d47ae50
Quentin Perret <qperret@google.com>
2020-07-13 13:37:28 +0100

    build.sh: Check expected modules after build
    In contexts such as GKI, it may be useful to keep an up-to-date list of
    modules built for a given configuration. To support such a use-case,
    introduce a new configuration option to build.sh called $MODULES_ORDER.
    
    $MODULES_ORDER must point to a file which will be compared with the
    modules.order file generated as part of the build, and any differences
    will be reported as a build error.
    
    Bug: 158655966
    Signed-off-by: Quentin Perret <qperret@google.com>
    Change-Id: I5275112b6c2bac288647f87b601f3da798564b4c

--- build.sh
+++ build.sh
@@ -158,6 +158,11 @@
 #     the contents of this variable, lines should be of the form: options
 #     <modulename> <param1>=<val> <param2>=<val> ...
 #
+#   MODULES_ORDER
+#     location of an optional file containing the list of modules that are
+#     expected to be built for the current configuration, in the modules.order
+#     format, relative to the kernel source tree.
+#
 #   LZ4_RAMDISK
 #     if defined, any ramdisks generated will be lz4 compressed instead of
 #     gzip compressed.
@@ -504,6 +509,17 @@ if [ -n "${POST_KERNEL_BUILD_CMDS}" ]; then
   set -x
   eval ${POST_KERNEL_BUILD_CMDS}
   set +x
+fi
+
+if [ -n "${MODULES_ORDER}" ]; then
+  echo "========================================================"
+  echo " Checking the list of modules:"
+  if ! diff -u "${KERNEL_DIR}/${MODULES_ORDER}" "${OUT_DIR}/modules.order"; then
+    echo "ERROR: modules list out of date" >&2
+    echo "Update it with:" >&2
+    echo "cp ${OUT_DIR}/modules.order ${KERNEL_DIR}/${MODULES_ORDER}" >&2
+    exit 1
+  fi
 fi
 
 if [ -n "${KMI_SYMBOL_LIST_STRICT_MODE}" ]; then
