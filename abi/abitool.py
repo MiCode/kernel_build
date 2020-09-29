reocat/android_kernel_build

abi: add --full-report option to generate verbose ABI reports
As of now the default reporting mode is one that is a tradeoff between
readability and verbosity. Add the --full-report option to get all the
gory details of an ABI breakage. This effectively removes the
--leaf-changes-only and --impacted-interfaces options from the `abidiff`
call. Note, the full report might take considerably longer to produce.

Bug: 158237359
Change-Id: I5aeba2733c072ed5977bed7f2f74dc6fd21ff257

abi/abitool.py
@@ -58,17 +58,22 @@ class Libabigail(AbiTool):
 
         subprocess.check_call(dump_abi_cmd)
 
-    def diff_abi(self, old_dump, new_dump, diff_report, short_report, whitelist):
+    def diff_abi(self, old_dump, new_dump, diff_report, short_report,
+                 whitelist, full_report):
         log.info('libabigail diffing: {} and {} at {}'.format(old_dump,
                                                                 new_dump,
                                                                 diff_report))
         diff_abi_cmd = ['abidiff',
-                        '--leaf-changes-only',
                         '--flag-indirect',
-                        '--impacted-interfaces',
                         '--dump-diff-tree',
                         old_dump,
                         new_dump]
+
+        if not full_report:
+            diff_abi_cmd.extend([
+                '--leaf-changes-only',
+                '--impacted-interfaces',
+            ])
 
         if whitelist is not None:
             diff_abi_cmd.extend(['--kmi-whitelist', whitelist])

build_abi.sh
@@ -46,11 +46,13 @@ function show_help {
     echo "  -u | --update         Update ABI representation and main whitelist in the source directory"
     echo "  -n | --nodiff         Do not generate an ABI report with diff_abi"
     echo "  -r | --print-report   Print ABI short report in case of any differences"
+    echo "  -f | --full-report    Create a detailled ABI report"
 }
 
 UPDATE=0
 DIFF=1
 PRINT_REPORT=0
+FULL_REPORT=0
 
 ARGS=()
 for i in "$@"
@@ -66,6 +68,10 @@ case $i in
     ;;
     -r|--print-report)
     PRINT_REPORT=1
+    shift # past argument=value
+    ;;
+    -f|--full-report)
+    FULL_REPORT=1
     shift # past argument=value
     ;;
     -h|--help)
@@ -222,11 +228,18 @@ if [ -n "$ABI_DEFINITION" ]; then
         echo "========================================================"
         echo " Comparing ABI against expected definition ($ABI_DEFINITION)"
         abi_report=${DIST_DIR}/abi.report
+
+        FULL_REPORT_FLAG=
+        if [ $FULL_REPORT -eq 1 ]; then
+            FULL_REPORT_FLAG="--full-report"
+        fi
+
         set +e
         ${ROOT_DIR}/build/abi/diff_abi --baseline $KERNEL_DIR/$ABI_DEFINITION \
                                        --new      ${DIST_DIR}/${abi_out_file} \
                                        --report   ${abi_report}               \
                                        --short-report ${abi_report}.short     \
+                                       $FULL_REPORT_FLAG                      \
                                        $KMI_WHITELIST_FLAG
         rc=$?
         set -e

MiCode/kernel_build

abi/abitool.py
@@ -58,17 +58,22 @@ class Libabigail(AbiTool):
 
         subprocess.check_call(dump_abi_cmd)
 
-    def diff_abi(self, old_dump, new_dump, diff_report, short_report, symbol_list):
+    def diff_abi(self, old_dump, new_dump, diff_report, short_report,
+                 symbol_list, full_report):
         log.info('libabigail diffing: {} and {} at {}'.format(old_dump,
                                                                 new_dump,
                                                                 diff_report))
         diff_abi_cmd = ['abidiff',
-                        '--leaf-changes-only',
                         '--flag-indirect',
-                        '--impacted-interfaces',
                         '--dump-diff-tree',
                         old_dump,
                         new_dump]
+
+        if not full_report:
+            diff_abi_cmd.extend([
+                '--leaf-changes-only',
+                '--impacted-interfaces',
+            ])
 
         if symbol_list is not None:
             diff_abi_cmd.extend(['--kmi-whitelist', symbol_list])

build_abi.sh
@@ -71,12 +71,14 @@ function show_help {
     echo "  -s | --update-symbol-list    Update main symbol list in the source directory"
     echo "  -n | --nodiff                Do not generate an ABI report with diff_abi"
     echo "  -r | --print-report          Print ABI short report in case of any differences"
+    echo "  -a | --full-report           Create a detailed ABI report"
 }
 
 UPDATE=0
 UPDATE_SYMBOL_LIST=0
 DIFF=1
 PRINT_REPORT=0
+FULL_REPORT=0
 
 if [[ -z "${KMI_SYMBOL_LIST_MODULE_GROUP}" ]]; then
   KMI_SYMBOL_LIST_MODULE_GROUPING=1
@@ -106,6 +108,10 @@ case $i in
     ;;
     -r|--print-report)
     PRINT_REPORT=1
+    shift # past argument=value
+    ;;
+    -a|--full-report)
+    FULL_REPORT=1
     shift # past argument=value
     ;;
     -h|--help)
@@ -299,11 +305,18 @@ if [ -n "$ABI_DEFINITION" ]; then
         echo "========================================================"
         echo " Comparing ABI against expected definition ($ABI_DEFINITION)"
         abi_report=${DIST_DIR}/abi.report
+
+        FULL_REPORT_FLAG=
+        if [ $FULL_REPORT -eq 1 ]; then
+            FULL_REPORT_FLAG="--full-report"
+        fi
+
         set +e
         ${ROOT_DIR}/build/abi/diff_abi --baseline $KERNEL_DIR/$ABI_DEFINITION \
                                        --new      ${DIST_DIR}/${abi_out_file} \
                                        --report   ${abi_report}               \
                                        --short-report ${abi_report}.short     \
+                                       $FULL_REPORT_FLAG                      \
                                        $KMI_SYMBOL_LIST_FLAG
         rc=$?
         set -e