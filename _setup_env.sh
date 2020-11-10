reocat/android_kernel_build

Add support for full hermetic toolchain restrictions
Setting "HERMETIC_TOOLCHAIN", will restrict the PATH to the defined
prebuilt directories as well as a small set of host tools that are known
to be required but not yet provided through the hermetic toolchain.

Change-Id: I83237811b522ed13de01fd17ae80fb0b431264f6
Signed-off-by: Matthias Maennich <maennich@google.com>

_setup_env.sh
@@ -88,6 +88,23 @@ LIBUFDT_PREBUILTS_BIN
 BUILDTOOLS_PREBUILT_BIN
 )
 
+if [ -n "${HERMETIC_TOOLCHAIN}" ]; then
+  HOST_TOOLS=${OUT_DIR}/host_tools
+  rm -rf ${HOST_TOOLS}
+  mkdir -p ${HOST_TOOLS}
+  for tool in \
+      git \
+      perl \
+      rsync \
+      sh \
+      tar \
+      ${ADDITIONAL_HOST_TOOLS}
+  do
+      ln -s $(which $tool) ${HOST_TOOLS}
+  done
+  PATH=${HOST_TOOLS}
+fi
+
 for PREBUILT_BIN in "${PREBUILTS_PATHS[@]}"; do
     PREBUILT_BIN=\${${PREBUILT_BIN}}
     eval PREBUILT_BIN="${PREBUILT_BIN}"

MiCode/kernel_build

_setup_env.sh
@@ -88,6 +88,23 @@ LIBUFDT_PREBUILTS_BIN
 BUILDTOOLS_PREBUILT_BIN
 )
 
+if [ -n "${HERMETIC_TOOLCHAIN}" ]; then
+  HOST_TOOLS=${OUT_DIR}/host_tools
+  rm -rf ${HOST_TOOLS}
+  mkdir -p ${HOST_TOOLS}
+  for tool in \
+      git \
+      perl \
+      rsync \
+      sh \
+      tar \
+      ${ADDITIONAL_HOST_TOOLS}
+  do
+      ln -sf $(which $tool) ${HOST_TOOLS}
+  done
+  PATH=${HOST_TOOLS}
+fi
+
 for PREBUILT_BIN in "${PREBUILTS_PATHS[@]}"; do
     PREBUILT_BIN=\${${PREBUILT_BIN}}
     eval PREBUILT_BIN="${PREBUILT_BIN}"