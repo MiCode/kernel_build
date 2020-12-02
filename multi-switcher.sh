354de345416f0dbfca2989327e8ef87439e3f9ea
Matthias Maennich <maennich@google.com>
2020-12-02 11:47:39 +0000

    multi-switcher: drop support for alternative 5.4 branch names
    This was transitional and is not longer needed.
    
    Signed-off-by: Matthias Maennich <maennich@google.com>
    Change-Id: I654571f3c4c2731b4f130f39f27a9ecc5a8ee3c3

--- multi-switcher.sh
+++ multi-switcher.sh
@@ -33,20 +33,12 @@ pushd $BASE > /dev/null
 
   echo "Switching to $BRANCH"
 
-  if [[ ${BRANCH} == "11-5.4" ]]; then
-    alt_name="5.4-stable"
-  fi
-
-  if [[ ${BRANCH} == "12-5.4" ]]; then
-    alt_name="5.4"
-  fi
-
   for dir in common common-modules/virtual-device; do
     if [ -L ${dir} ]; then
       rm ${dir}
     fi
 
-    for candidate in ${dir}-${BRANCH} ${dir}-${alt_name} ${dir}${BRANCH}; do
+    for candidate in ${dir}-${BRANCH} ${dir}${BRANCH}; do
       if [ -d ${candidate} ]; then
           (
             cd $(dirname $candidate)
