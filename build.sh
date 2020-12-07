c91aec1808ca4e0229b05e7bfe3798d3702792e8
Howard Chen <howardsoc@google.com>
2020-12-07 16:18:18 +0800

    Refine the usage document
    Add a use case for the BUILD_CONFIG.
    
    Test: BUILD_CONFIG=common/build.config.gki.aarch64 build/build.sh
    Change-Id: I5037f07a09619ff1fbd9d9ec249ab5387c06f03b

--- build.sh
+++ build.sh
@@ -17,11 +17,16 @@
 # Usage:
 #   build/build.sh <make options>*
 # or:
-#   OUT_DIR=<out dir> DIST_DIR=<dist dir> build/build.sh <make options>*
+#   To define custom out and dist directories:
+#     OUT_DIR=<out dir> DIST_DIR=<dist dir> build/build.sh <make options>*
+#   To use a custom build config:
+#     BUILD_CONFIG=<path to the build.config> <make options>*
 #
-# Example:
-#   OUT_DIR=output DIST_DIR=dist build/build.sh -j24 V=1
-#
+# Examples:
+#   To define custom out and dist directories:
+#     OUT_DIR=output DIST_DIR=dist build/build.sh -j24 V=1
+#   To use a custom build config:
+#     BUILD_CONFIG=common/build.config.gki.aarch64 build/build.sh -j24 V=1
 #
 # The following environment variables are considered during execution:
 #
