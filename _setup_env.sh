7447364b34a81e9f776f7a61123c0e8282676d53
Matthias Maennich <maennich@google.com>
2021-06-18 15:54:00 +0100

    _setup_env.sh: actually export _SETUP_ENV_SH_INCLUDED=1
    The include guard _SETUP_ENV_SH_INCLUDED was only effective if used with
      $ source _setup_env.sh
    
    In an environment where you actually want to capture effective
    environment, an export is required to prevent double execution of this
    setup. Hence, do the export.
    
    Bug: 189451175
    Signed-off-by: Matthias Maennich <maennich@google.com>
    Change-Id: I1a027e354e840d012f57935ed2e2e14636635045

--- _setup_env.sh
+++ _setup_env.sh
@@ -18,7 +18,7 @@
 # a shell, please let kernel-team@android.com know and we are happy to help
 # with your use case.
 
-[ -n "$_SETUP_ENV_SH_INCLUDED" ] && return || _SETUP_ENV_SH_INCLUDED=1
+[ -n "$_SETUP_ENV_SH_INCLUDED" ] && return || export _SETUP_ENV_SH_INCLUDED=1
 
 # TODO: Use a $(gettop) style method.
 export ROOT_DIR=$(readlink -f $PWD)
