reocat/android_kernel_build

kleaf: Add targets for prebuilts.
define_common_kernels() invokes _define_prebuilts().

_define_prebuilts() defines:
- a few targets named with suffix `_downloaded`.
  If `--use_prebuilt_gki` is set, these targets are valid, and they
  will download from ci.android.com
- a few targets named with suffix `_download_or_build`, such that:
  - If `--use_prebuilt_gki` is set, points to `_downloaded`
  - Otherwise build from source.

Also update bazel.py to recognize --use_prebuilt_gki.

Also update docstrings and error messages to hide
KLEAF_DOWNLOAD_BUILD_NUMBER_MAP as much as possible.

Bug: 206079661
Test: manual

Change-Id: I92e265af28f25be9c3378a6eca71225f84ca3280
Signed-off-by: Yifan Hong <elsk@google.com>

kleaf/bazel.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 import functools
 import os
 import subprocess
@@ -36,8 +37,9 @@ def check_output(*args, **kwargs):
 def check_call(*args, **kwargs):
     return exec_silent(functools.partial(subprocess.check_call, *args, **kwargs))
 
-
 def main(argv, env):
+    env = env.copy()
+
     root_dir = env["ROOT_DIR"]
 
     source_date_epoch = check_output(
@@ -53,7 +55,19 @@ def main(argv, env):
 
     absolute_out_dir = "{root_dir}/out".format(root_dir=root_dir)
 
-    args = [
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("--use_prebuilt_gki")
+    known_args, remaining_args = parser.parse_known_args(argv[1:])
+    if known_args.use_prebuilt_gki:
+        # Insert before positional arguments
+        try:
+            idx = remaining_args.index("--")
+        except ValueError:
+            idx = len(remaining_args)
+        remaining_args.insert(idx, "--//common:use_prebuilt_gki")
+        env["KLEAF_DOWNLOAD_BUILD_NUMBER_MAP"] = "gki_prebuilts=" + known_args.use_prebuilt_gki
+
+    bazel_args = [
         bazel_path,
         "--server_javabase={}".format(bazel_jdk_path),
         "--output_user_root={}/bazel/output_user_root".format(absolute_out_dir),
@@ -63,12 +77,11 @@ def main(argv, env):
             root_dir=root_dir,
             bazelrc_name=bazelrc_name)
     ]
-    args += argv[1:]
+    bazel_args += remaining_args
 
-    env = env.copy()
     env["SOURCE_DATE_EPOCH"] = source_date_epoch
 
-    check_call(args, env=env)
+    check_call(bazel_args, env=env)
 
 
 if __name__ == "__main__":

MiCode/kernel_build

kleaf/bazel.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 import os
 import subprocess
 import sys
@@ -44,6 +45,18 @@ def main(root_dir, bazel_args, env):
     bazelrc_name = "build/kernel/kleaf/common.bazelrc"
 
     absolute_out_dir = "{root_dir}/out".format(root_dir=root_dir)
+
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("--use_prebuilt_gki")
+    known_args, bazel_args = parser.parse_known_args(bazel_args)
+    if known_args.use_prebuilt_gki:
+        # Insert before positional arguments
+        try:
+            idx = bazel_args.index("--")
+        except ValueError:
+            idx = len(bazel_args)
+        bazel_args.insert(idx, "--//common:use_prebuilt_gki")
+        env["KLEAF_DOWNLOAD_BUILD_NUMBER_MAP"] = "gki_prebuilts=" + known_args.use_prebuilt_gki
 
     command_args = [
         bazel_path,