e817119390886e5ef80ede7a4d2c8373a57e09db
Lucas Wei <lucaswei@google.com>
2022-06-01 15:29:23 +0800

    build/kernel: Generate vendor_kernel_boot.modules.load
    After we supporting build vendor_kernel_boot image for kernel
    artifacts, we should also update file name of *.modules.load
    with its related image name.
    
    Bug: 234554094
    Signed-off-by: Lucas Wei <lucaswei@google.com>
    Change-Id: Id26e7d692b13a8fd19b5db5a7c6a7d15277e569a

--- build.sh
+++ build.sh
@@ -1016,7 +1016,11 @@ if [ -n "${MODULES}" ]; then
 
     MODULES_ROOT_DIR=$(echo ${INITRAMFS_STAGING_DIR}/lib/modules/*)
     cp ${MODULES_ROOT_DIR}/modules.load ${DIST_DIR}/modules.load
-    cp ${MODULES_ROOT_DIR}/modules.load ${DIST_DIR}/vendor_boot.modules.load
+    if [ -n "${BUILD_VENDOR_BOOT_IMG}" ]; then
+      cp ${MODULES_ROOT_DIR}/modules.load ${DIST_DIR}/vendor_boot.modules.load
+    elif [ -n "${BUILD_VENDOR_KERNEL_BOOT}" ]; then
+      cp ${MODULES_ROOT_DIR}/modules.load ${DIST_DIR}/vendor_kernel_boot.modules.load
+    fi
     echo "${MODULES_OPTIONS}" > ${MODULES_ROOT_DIR}/modules.options
 
     mkbootfs "${INITRAMFS_STAGING_DIR}" >"${MODULES_STAGING_DIR}/initramfs.cpio"
