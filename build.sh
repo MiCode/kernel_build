fd8c63df761b92bffc087570cd39ccb5f7e6ed6e
Lucas Wei <lucaswei@google.com>
2022-06-29 16:28:09 +0800

    Fix build vendor_kernel_boot.img
    Add condition if we only want to build vendor_kernel_boot.img
    when enabling BUILD_VENDOR_KERNEL_BOOT=1.
    
    Bug: 237436066
    Signed-off-by: Lucas Wei <lucaswei@google.com>
    Change-Id: I4519ffa231e10dfc2c289da1f1ef23b8fcd8eeda

--- build.sh
+++ build.sh
@@ -1081,7 +1081,8 @@ fi
 echo "========================================================"
 echo " Files copied to ${DIST_DIR}"
 
-if [ -n "${BUILD_BOOT_IMG}" -o -n "${BUILD_VENDOR_BOOT_IMG}" ] ; then
+if [ -n "${BUILD_BOOT_IMG}" -o -n "${BUILD_VENDOR_BOOT_IMG}" \
+      -o -n "${BUILD_VENDOR_KERNEL_BOOT}" ] ; then
   build_boot_images
 fi
 
