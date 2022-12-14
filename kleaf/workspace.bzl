reocat/android_kernel_build

kleaf: Stub @remote_coverage_tools
Stub out @remote_coverage_tools that are downloaded for testing.

Test: bazel build --nobuild //common:kernel_aarch64_tests --experimental_repository_disable_download
Bug: 261489408
Change-Id: Iacea611097b162410553f2833917291f101bc56b

kleaf/workspace.bzl
@@ -91,6 +91,12 @@ def define_kleaf_workspace(common_kernel_package = None):
         path = "build/kernel/kleaf/impl/fake_rules_cc",
     )
 
+    # Stub out @remote_coverage_tools required for testing.
+    native.local_repository(
+        name = "remote_coverage_tools",
+        path = "build/bazel/rules/coverage/remote_coverage_tools",
+    )
+
     native.register_toolchains(
         "//prebuilts/build-tools:py_toolchain",
     )

MiCode/kernel_build

kleaf/workspace.bzl
@@ -91,6 +91,12 @@ def define_kleaf_workspace(common_kernel_package = None):
         path = "build/kernel/kleaf/impl/fake_rules_cc",
     )
 
+    # Stub out @remote_coverage_tools required for testing.
+    native.local_repository(
+        name = "remote_coverage_tools",
+        path = "build/bazel_common_rules/rules/coverage/remote_coverage_tools",
+    )
+
     native.register_toolchains(
         "//prebuilts/build-tools:py_toolchain",
     )