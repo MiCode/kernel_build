reocat/android_kernel_build

kleaf: run modules_prepare.setup before kernel_build setup
... when building external modules.

Before this change, kernel_build[KernelEnvInfo].setup restores
$OUT_DIR/scripts/sign-file and various others as symlinks,
then modules_prepare out_dir is restored, overwriting
$OUT_DIR/scripts/sign-file as a file.

This works for the host GNU tar. However, with the prebuilt toybox tar,
it follows $OUT_DIR/scripts/sign-file to a readonly file, and
notice that the symlink goes outside of the decompress directory,
throwing an error.

Hence, to prevent this behavior, do the following in this order before
building external modules:

1. set up environment variables (kernel_env[KernelEnvInfo].setup)
2. Restore configs
   (kernel_config[KernelConfigEnvInfo].post_env_info.setup)
3. Restore modules_prepare results (modules_prepare[KernelEnvInfo.setup)
4. Restore kernel_build's outputs

Test: TH
Test: build CF manually with --config=local
Bug: 243570975

Change-Id: Ic44dba376adbada1c26f191145616a276168593f

kleaf/impl/kernel_module.bzl
@@ -270,8 +270,7 @@ def _kernel_module_impl(ctx):
         fail("{}: must not define `makefile` for `ddk_module`")
 
     inputs = []
-    inputs += ctx.attr.kernel_build[KernelEnvInfo].dependencies
-    inputs += ctx.attr.kernel_build[KernelBuildExtModuleInfo].modules_prepare_deps
+    inputs += ctx.attr.kernel_build[KernelBuildExtModuleInfo].env_info.dependencies
     inputs += ctx.files.makefile
     inputs += ctx.files.internal_ddk_makefiles_dir
     inputs += [
@@ -338,9 +337,7 @@ def _kernel_module_impl(ctx):
     if unstripped_dir:
         command_outputs.append(unstripped_dir)
 
-    command = ""
-    command += ctx.attr.kernel_build[KernelEnvInfo].setup
-    command += ctx.attr.kernel_build[KernelBuildExtModuleInfo].modules_prepare_setup
+    command = ctx.attr.kernel_build[KernelBuildExtModuleInfo].modules_env_info.setup
     command += """
              # create dirs for modules
                mkdir -p {kernel_uapi_headers_dir}/usr
@@ -587,7 +584,7 @@ _kernel_module = rule(
         "internal_exclude_kernel_build_module_srcs": attr.bool(),
         "kernel_build": attr.label(
             mandatory = True,
-            providers = [KernelEnvInfo, KernelBuildExtModuleInfo],
+            providers = [KernelBuildExtModuleInfo],
         ),
         "deps": attr.label_list(),
         # Not output_list because it is not a list of labels. The list of

kleaf/impl/kernel_modules_install.bzl
@@ -50,8 +50,7 @@ def _kernel_modules_install_impl(ctx):
     modules_staging_dws_list = modules_staging_dws_depset.to_list()
 
     inputs = []
-    inputs += kernel_build[KernelEnvInfo].dependencies
-    inputs += kernel_build[KernelBuildExtModuleInfo].modules_prepare_deps
+    inputs += kernel_build[KernelBuildExtModuleInfo].env_info.dependencies
     inputs += [
         ctx.file._search_and_cp_output,
         ctx.file._check_duplicated_files_in_archives,
@@ -73,9 +72,7 @@ def _kernel_modules_install_impl(ctx):
 
     modules_staging_dws = dws.make(ctx, "{}/staging".format(ctx.label.name))
 
-    command = ""
-    command += kernel_build[KernelEnvInfo].setup
-    command += kernel_build[KernelBuildExtModuleInfo].modules_prepare_setup
+    command = ctx.attr.kernel_build[KernelBuildExtModuleInfo].modules_env_info.setup
     command += """
              # create dirs for modules
                mkdir -p {modules_staging_dir}
@@ -209,7 +206,7 @@ In `foo_dist`, specifying `foo_modules_install` in `data` won't include
             doc = "A list of labels referring to `kernel_module`s to install.",
         ),
         "kernel_build": attr.label(
-            providers = [KernelEnvInfo, KernelBuildExtModuleInfo],
+            providers = [KernelBuildExtModuleInfo],
             doc = "Label referring to the `kernel_build` module. Otherwise, it" +
                   " is inferred from `kernel_modules`.",
         ),

MiCode/kernel_build

kleaf/impl/kernel_module.bzl
@@ -264,8 +264,7 @@ def _kernel_module_impl(ctx):
         fail("{}: must not define `makefile` for `ddk_module`")
 
     inputs = []
-    inputs += ctx.attr.kernel_build[KernelEnvInfo].dependencies
-    inputs += ctx.attr.kernel_build[KernelBuildExtModuleInfo].modules_prepare_deps
+    inputs += ctx.attr.kernel_build[KernelBuildExtModuleInfo].env_info.dependencies
     inputs += ctx.files.makefile
     inputs += ctx.files.internal_ddk_makefiles_dir
     inputs += [
@@ -332,9 +331,7 @@ def _kernel_module_impl(ctx):
     if unstripped_dir:
         command_outputs.append(unstripped_dir)
 
-    command = ""
-    command += ctx.attr.kernel_build[KernelEnvInfo].setup
-    command += ctx.attr.kernel_build[KernelBuildExtModuleInfo].modules_prepare_setup
+    command = ctx.attr.kernel_build[KernelBuildExtModuleInfo].env_info.setup
     command += """
              # create dirs for modules
                mkdir -p {kernel_uapi_headers_dir}/usr
@@ -581,7 +578,7 @@ _kernel_module = rule(
         "internal_exclude_kernel_build_module_srcs": attr.bool(),
         "kernel_build": attr.label(
             mandatory = True,
-            providers = [KernelEnvInfo, KernelBuildExtModuleInfo],
+            providers = [KernelBuildExtModuleInfo],
         ),
         "deps": attr.label_list(),
         # Not output_list because it is not a list of labels. The list of

kleaf/impl/kernel_modules_install.bzl
@@ -20,7 +20,6 @@ load(
     ":common_providers.bzl",
     "KernelBuildExtModuleInfo",
     "KernelCmdsInfo",
-    "KernelEnvInfo",
     "KernelModuleInfo",
 )
 load(":debug.bzl", "debug")
@@ -50,8 +49,7 @@ def _kernel_modules_install_impl(ctx):
     modules_staging_dws_list = modules_staging_dws_depset.to_list()
 
     inputs = []
-    inputs += kernel_build[KernelEnvInfo].dependencies
-    inputs += kernel_build[KernelBuildExtModuleInfo].modules_prepare_deps
+    inputs += kernel_build[KernelBuildExtModuleInfo].env_info.dependencies
     inputs += [
         ctx.file._search_and_cp_output,
         ctx.file._check_duplicated_files_in_archives,
@@ -73,9 +71,7 @@ def _kernel_modules_install_impl(ctx):
 
     modules_staging_dws = dws.make(ctx, "{}/staging".format(ctx.label.name))
 
-    command = ""
-    command += kernel_build[KernelEnvInfo].setup
-    command += kernel_build[KernelBuildExtModuleInfo].modules_prepare_setup
+    command = ctx.attr.kernel_build[KernelBuildExtModuleInfo].env_info.setup
     command += """
              # create dirs for modules
                mkdir -p {modules_staging_dir}
@@ -209,7 +205,7 @@ In `foo_dist`, specifying `foo_modules_install` in `data` won't include
             doc = "A list of labels referring to `kernel_module`s to install.",
         ),
         "kernel_build": attr.label(
-            providers = [KernelEnvInfo, KernelBuildExtModuleInfo],
+            providers = [KernelBuildExtModuleInfo],
             doc = "Label referring to the `kernel_build` module. Otherwise, it" +
                   " is inferred from `kernel_modules`.",
         ),