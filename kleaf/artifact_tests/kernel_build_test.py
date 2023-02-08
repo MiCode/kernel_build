8b3ed629ffb5441982b31374603a565b678d265c
Matthias Maennich <maennich@google.com>
2023-02-08 12:57:44 +0000

    kernel_build_test: pyformat to get a consistent style
    Change-Id: I5e1176c9ab2f80c038ab054695c8ae75f4ac9988
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- kleaf/artifact_tests/kernel_build_test.py
+++ kleaf/artifact_tests/kernel_build_test.py
@@ -21,39 +21,41 @@ import unittest
 
 from absl.testing import absltest
 
+
 def load_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--strings", default="strings")
-    parser.add_argument("--artifacts", nargs="*", default=[])
-    return parser.parse_known_args()
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--strings", default="strings")
+  parser.add_argument("--artifacts", nargs="*", default=[])
+  return parser.parse_known_args()
 
 
 arguments = None
 
 
 class ScmVersionTestCase(unittest.TestCase):
-    # Version.PatchLevel.SubLevel-AndroidRelease-KmiGeneration[-Tag]-Sha1
-    # e.g. 5.4.42-android12-0-00544-ged21d463f856
-    # e.g. 5.4.42-mainline-00544-ged21d463f856
-    # e.g. 5.18.0-rc3-mainline-19648-g9d2f688e65db
-    _scmversion_pattern = re.compile(
-        r"Linux version [0-9]+[.][0-9]+[.][0-9]+(-android[0-9]+-[0-9]+|(-rc[0-9]+)?-mainline)(-[0-9]+)?-g[0-9a-f]{12,40}")
+  # Version.PatchLevel.SubLevel-AndroidRelease-KmiGeneration[-Tag]-Sha1
+  # e.g. 5.4.42-android12-0-00544-ged21d463f856
+  # e.g. 5.4.42-mainline-00544-ged21d463f856
+  # e.g. 5.18.0-rc3-mainline-19648-g9d2f688e65db
+  _scmversion_pattern = re.compile(
+      r"Linux version [0-9]+[.][0-9]+[.][0-9]+(-android[0-9]+-[0-9]+|(-rc[0-9]+)?-mainline)(-[0-9]+)?-g[0-9a-f]{12,40}"
+  )
 
-    def test_vmlinux_contains_scmversion(self):
-        """Test that vmlinux (if exists) has scmversion."""
-        for artifact in arguments.artifacts:
-            if os.path.basename(artifact) != "vmlinux":
-                continue
-            strings = subprocess.check_output([arguments.strings, artifact],
-                                              text=True).strip().splitlines()
-            matches = any(ScmVersionTestCase._scmversion_pattern.search(s)
-                          for s in strings)
-            msg = "scmversion not found for vmlinux, found {}".format(
-                [s for s in strings if "Linux version" in s]
-            )
-            self.assertTrue(matches, msg)
+  def test_vmlinux_contains_scmversion(self):
+    """Test that vmlinux (if exists) has scmversion."""
+    for artifact in arguments.artifacts:
+      if os.path.basename(artifact) != "vmlinux":
+        continue
+      strings = subprocess.check_output([arguments.strings, artifact],
+                                        text=True).strip().splitlines()
+      matches = any(
+          ScmVersionTestCase._scmversion_pattern.search(s) for s in strings)
+      msg = "scmversion not found for vmlinux, found {}".format(
+          [s for s in strings if "Linux version" in s])
+      self.assertTrue(matches, msg)
 
-if __name__ == '__main__':
-    arguments, unknown = load_arguments()
-    sys.argv[1:] = unknown
-    absltest.main()
+
+if __name__ == "__main__":
+  arguments, unknown = load_arguments()
+  sys.argv[1:] = unknown
+  absltest.main()
