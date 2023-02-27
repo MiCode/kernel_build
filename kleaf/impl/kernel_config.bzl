reocat/android_kernel_build

kleaf: Select best lto option for fast config.
* Currently LTO is set to thin for fast config,
however this is not the best path in non LTO branches.

* This removes the assumption that lto=thin is the
fastest option and instead delegates its setting to
the kernel_config rule.

Bug: 270138023
Change-Id: Id0e9f7fc18d48e2bf8f2f9eb344f932d756036f7
Signed-off-by: Ulises Mendez Martinez <umendez@google.com>

kleaf/impl/kernel_config.bzl
@@ -156,6 +156,18 @@ def _config_lto(ctx):
             _config.enable("LTO_CLANG_FULL"),
             _config.disable("THINLTO"),
         ]
+        # Try optimizing iff there was no value provided.
+
+    elif lto_config_flag == "default":
+        # Set lto=thin only if LTO is a thing.
+        if ctx.attr._config_is_fast[BuildSettingInfo].value:
+            lto_configs += [
+                _config.enable_if("LTO", "LTO_CLANG"),
+                _config.disable_if("LTO", "LTO_NONE"),
+                _config.enable_if("LTO", "LTO_CLANG_THIN"),
+                _config.disable_if("LTO", "LTO_CLANG_FULL"),
+                _config.enable_if("LTO", "THINLTO"),
+            ]
 
     return struct(configs = lto_configs, deps = [])
 
@@ -501,6 +513,7 @@ kernel_config = rule(
         "_cache_dir": attr.label(default = "//build/kernel/kleaf:cache_dir"),
         "_hermetic_tools": attr.label(default = "//build/kernel:hermetic-tools", providers = [HermeticToolsInfo]),
         "_config_is_local": attr.label(default = "//build/kernel/kleaf:config_local"),
+        "_config_is_fast": attr.label(default = "//build/kernel/kleaf:config_fast"),
         "_config_is_stamp": attr.label(default = "//build/kernel/kleaf:config_stamp"),
         "_debug_print_scripts": attr.label(default = "//build/kernel/kleaf:debug_print_scripts"),
         "_allowlist_function_transition": attr.label(

MiCode/kernel_build

kleaf/impl/kernel_config.bzl
@@ -156,6 +156,15 @@ def _config_lto(ctx):
             _config.enable("LTO_CLANG_FULL"),
             _config.disable("THINLTO"),
         ]
+    elif lto_config_flag == "fast":
+        # Set lto=thin only if LTO full is enabled.
+        lto_configs += [
+            _config.enable_if("LTO_CLANG_FULL", "LTO_CLANG"),
+            _config.disable_if("LTO_CLANG_FULL", "LTO_NONE"),
+            _config.enable_if("LTO_CLANG_FULL", "LTO_CLANG_THIN"),
+            _config.enable_if("LTO_CLANG_FULL", "THINLTO"),
+            _config.disable_if("LTO_CLANG_FULL", "LTO_CLANG_FULL"),
+        ]
 
     return struct(configs = lto_configs, deps = [])
 