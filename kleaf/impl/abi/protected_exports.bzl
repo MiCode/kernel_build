reocat/android_kernel_build

kleaf: Add support to update protected exports list
Adds a new ABI target: <target_name>_abi_update_protected_exports

"extract_gki_protected_exports.py" is being incorporated as a "kleaf ABI"
target, with the purpose of updating the list of protected exports. These
exports refer to symbols that are exported by modules that need to be
considered in protected class i.e. unsigned modules can not exports these
symbols. The list to update is set to "protected_exports_list"
attribute. The list of modules to be considered as protected is passed as
"protected_modules_list" attribute.

In the case of GKI kernels, the "protected_exports_list" attribute is typically
named "android/abi_gki_protected_exports", while the "protected_modules_list"
attribute is named "android/gki_protected_modules". These attributes define the
list of protected exports and modules, respectively.

Bug: 26867921
Test: TH
Test: bazel run //common:kernel_aarch64_abi_update_protected_exports
Change-Id: Ic8edec43faf7695cd6509c93d428ea8b06c1c334
Signed-off-by: Ramji Jiyani <ramjiyani@google.com>

kleaf/impl/abi/protected_exports.bzl
@@ -1,0 +1,109 @@
+# Copyright (C) 2023 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Extracts protected exports from protected kernel modules."""
+
+load(":abi/abi_transitions.bzl", "notrim_transition")
+load(
+    ":common_providers.bzl",
+    "KernelBuildAbiInfo",
+    "KernelEnvAndOutputsInfo",
+    "KernelModuleInfo",
+)
+load(":debug.bzl", "debug")
+load(":utils.bzl", "utils")
+
+def _protected_exports_impl(ctx):
+    if ctx.attr.kernel_build_notrim[KernelBuildAbiInfo].trim_nonlisted_kmi:
+        fail("{}: Requires `kernel_build` {} to have `trim_nonlisted_kmi = False`.".format(
+            ctx.label,
+            ctx.attr.kernel_build_notrim.label,
+        ))
+
+    if not ctx.attr.protected_modules_list:
+        fail("{}: requires protected_modules_list.".format(ctx.label))
+
+    out = ctx.actions.declare_file("{}/protected_exports".format(ctx.attr.name))
+    intermediates_dir = utils.intermediates_dir(ctx)
+
+    inputs = [
+        ctx.file.protected_modules_list,
+    ]
+    transitive_inputs = [ctx.attr.kernel_build_notrim[KernelEnvAndOutputsInfo].inputs]
+    tools = [ctx.executable._extract_protected_exports]
+    transitive_tools = [ctx.attr.kernel_build_notrim[KernelEnvAndOutputsInfo].tools]
+
+    flags = ["--protected-exports-list", out.path]
+    flags += ["--gki-protected-modules-list", ctx.file.protected_modules_list.path]
+
+    # Get the signed and stripped module archive for the GKI modules
+    base_modules_archive = ctx.attr.kernel_build_notrim[KernelBuildAbiInfo].base_modules_staging_archive
+    if not base_modules_archive:
+        base_modules_archive = ctx.attr.kernel_build_notrim[KernelBuildAbiInfo].modules_staging_archive
+    inputs.append(base_modules_archive)
+
+    command = ctx.attr.kernel_build_notrim[KernelEnvAndOutputsInfo].get_setup_script(
+        data = ctx.attr.kernel_build_notrim[KernelEnvAndOutputsInfo].data,
+        restore_out_dir_cmd = utils.get_check_sandbox_cmd(),
+    )
+    command += """
+        mkdir -p {intermediates_dir}
+        mkdir -p {intermediates_dir}/temp
+        # Archive layout is lib/modules/<kernel_version>/kernel/<modules tree>
+        # Only extract kernel and below to get rid of variable <kernel version>
+        tar xf {base_modules_archive} --strip-components=4 -C {intermediates_dir}/temp
+        # We only need to match the <module tree> under kernel against protected modules list
+        mv {intermediates_dir}/temp/kernel/* {intermediates_dir}/
+        rm -rf {intermediates_dir}/temp
+        {extract_protected_exports} {flags} {intermediates_dir}
+        rm -rf {intermediates_dir}
+    """.format(
+        intermediates_dir = intermediates_dir,
+        extract_protected_exports = ctx.executable._extract_protected_exports.path,
+        flags = " ".join(flags),
+        base_modules_archive = base_modules_archive.path,
+    )
+    debug.print_scripts(ctx, command)
+    ctx.actions.run_shell(
+        inputs = depset(inputs, transitive = transitive_inputs),
+        outputs = [out],
+        command = command,
+        tools = depset(tools, transitive = transitive_tools),
+        progress_message = "Extracting protected exports {}".format(ctx.label),
+        mnemonic = "KernelProtectedExports",
+    )
+
+    return DefaultInfo(files = depset([out]))
+
+protected_exports = rule(
+    implementation = _protected_exports_impl,
+    attrs = {
+        # We can't use kernel_filegroup + hermetic_tools here because
+        # - extract_protected_exports depends on the clang toolchain, which requires us to
+        #   know the toolchain_version ahead of time.
+        # - We also don't have the necessity to extract symbols from prebuilts.
+        "kernel_build_notrim": attr.label(providers = [KernelEnvAndOutputsInfo, KernelBuildAbiInfo]),
+        "protected_modules_list": attr.label(doc = "List of protected modules whose exports needs to be extracted.", allow_single_file = True),
+        "_extract_protected_exports": attr.label(
+            default = "//build/kernel:extract_protected_exports",
+            cfg = "exec",
+            executable = True,
+        ),
+        "_debug_print_scripts": attr.label(default = "//build/kernel/kleaf:debug_print_scripts"),
+        "_allowlist_function_transition": attr.label(
+            default = "@bazel_tools//tools/allowlists/function_transition_allowlist",
+        ),
+    },
+    cfg = notrim_transition,
+)

MiCode/kernel_build

kleaf/impl/abi/protected_exports.bzl
@@ -1,0 +1,109 @@
+# Copyright (C) 2023 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Extracts protected exports from protected kernel modules."""
+
+load(":abi/abi_transitions.bzl", "notrim_transition")
+load(
+    ":common_providers.bzl",
+    "KernelBuildAbiInfo",
+    "KernelEnvAndOutputsInfo",
+    "KernelModuleInfo",
+)
+load(":debug.bzl", "debug")
+load(":utils.bzl", "utils")
+
+def _protected_exports_impl(ctx):
+    if ctx.attr.kernel_build[KernelBuildAbiInfo].trim_nonlisted_kmi:
+        fail("{}: Requires `kernel_build` {} to have `trim_nonlisted_kmi = False`.".format(
+            ctx.label,
+            ctx.attr.kernel_build.label,
+        ))
+
+    if not ctx.file.protected_modules_list_file:
+        fail("{}: {} is not a file.".format(ctx.label, ctx.file.protected_modules_list_file))
+
+    out = ctx.actions.declare_file("{}/protected_exports".format(ctx.attr.name))
+    intermediates_dir = utils.intermediates_dir(ctx)
+
+    inputs = [
+        ctx.file.protected_modules_list_file,
+    ]
+    transitive_inputs = [ctx.attr.kernel_build[KernelEnvAndOutputsInfo].inputs]
+    tools = [ctx.executable._extract_protected_exports]
+    transitive_tools = [ctx.attr.kernel_build[KernelEnvAndOutputsInfo].tools]
+
+    flags = ["--protected-exports-list", out.path]
+    flags += ["--gki-protected-modules-list", ctx.file.protected_modules_list_file.path]
+
+    # Get the signed and stripped module archive for the GKI modules
+    base_modules_archive = ctx.attr.kernel_build[KernelBuildAbiInfo].base_modules_staging_archive
+    if not base_modules_archive:
+        base_modules_archive = ctx.attr.kernel_build[KernelBuildAbiInfo].modules_staging_archive
+    inputs.append(base_modules_archive)
+
+    command = ctx.attr.kernel_build[KernelEnvAndOutputsInfo].get_setup_script(
+        data = ctx.attr.kernel_build[KernelEnvAndOutputsInfo].data,
+        restore_out_dir_cmd = utils.get_check_sandbox_cmd(),
+    )
+    command += """
+        mkdir -p {intermediates_dir}
+        mkdir -p {intermediates_dir}/temp
+        # Archive layout is lib/modules/<kernel_version>/kernel/<modules tree>
+        # Only extract kernel and below to get rid of variable <kernel version>
+        tar xf {base_modules_archive} --strip-components=4 -C {intermediates_dir}/temp
+        # We only need to match the <module tree> under kernel against protected modules list
+        mv {intermediates_dir}/temp/kernel/* {intermediates_dir}/
+        rm -rf {intermediates_dir}/temp
+        {extract_protected_exports} {flags} {intermediates_dir}
+        rm -rf {intermediates_dir}
+    """.format(
+        intermediates_dir = intermediates_dir,
+        extract_protected_exports = ctx.executable._extract_protected_exports.path,
+        flags = " ".join(flags),
+        base_modules_archive = base_modules_archive.path,
+    )
+    debug.print_scripts(ctx, command)
+    ctx.actions.run_shell(
+        inputs = depset(inputs, transitive = transitive_inputs),
+        outputs = [out],
+        command = command,
+        tools = depset(tools, transitive = transitive_tools),
+        progress_message = "Extracting protected exports {}".format(ctx.label),
+        mnemonic = "KernelProtectedExports",
+    )
+
+    return DefaultInfo(files = depset([out]))
+
+protected_exports = rule(
+    implementation = _protected_exports_impl,
+    attrs = {
+        # We can't use kernel_filegroup + hermetic_tools here because
+        # - extract_protected_exports depends on the clang toolchain, which requires us to
+        #   know the toolchain_version ahead of time.
+        # - We also don't have the necessity to extract symbols from prebuilts.
+        "kernel_build": attr.label(providers = [KernelEnvAndOutputsInfo, KernelBuildAbiInfo]),
+        "protected_modules_list_file": attr.label(doc = "List of protected modules whose exports needs to be extracted.", allow_single_file = True),
+        "_extract_protected_exports": attr.label(
+            default = "//build/kernel:extract_protected_exports",
+            cfg = "exec",
+            executable = True,
+        ),
+        "_debug_print_scripts": attr.label(default = "//build/kernel/kleaf:debug_print_scripts"),
+        "_allowlist_function_transition": attr.label(
+            default = "@bazel_tools//tools/allowlists/function_transition_allowlist",
+        ),
+    },
+    cfg = notrim_transition,
+)