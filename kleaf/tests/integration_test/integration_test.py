6b23e557adfdc6aa85f8bb31becd98ab1f247c2d
Yifan Hong <elsk@google.com>
2023-03-08 18:26:42 -0800

    kleaf: integration test: add restore_file_after_test function.
    Refactor useful function for future use.
    
    Change-Id: I79513089b77ce3a2443a10f1cac7284561ef247a

--- kleaf/tests/integration_test/integration_test.py
+++ kleaf/tests/integration_test/integration_test.py
@@ -64,6 +64,7 @@ _FASTEST = _LOCAL + _LTO_NONE
 
 _INTEGRATION_TEST_BAZEL_RC = "out/bazel/integration_test.bazelrc"
 
+
 def load_arguments():
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class=argparse.RawTextHelpFormatter)
@@ -95,7 +96,7 @@ class Exec(object):
 
 class KleafIntegrationTest(unittest.TestCase):
     def _check_call(self, command: str, command_args: list[str],
-                    startup_options = (),
+                    startup_options=(),
                     **kwargs) -> None:
         """Executes a bazel command."""
         startup_options = list(startup_options)
@@ -103,25 +104,23 @@ class KleafIntegrationTest(unittest.TestCase):
         command_args = list(command_args)
         command_args.extend(arguments.bazel_wrapper_args)
         Exec.check_call([str(_BAZEL)] + startup_options + [
-                         command,
-                        ] + command_args, **kwargs)
-
+            command,
+        ] + command_args, **kwargs)
 
     def _build(self, command_args: list[str], **kwargs) -> None:
         """Executes a bazel build command."""
         self._check_call("build", command_args, **kwargs)
 
-
     def _check_output(self, command: str, command_args: list[str], **kwargs) -> str:
         """Returns output of a bazel command."""
         return Exec.check_output([str(_BAZEL),
-                         f"--bazelrc={self._bazel_rc.name}",
-                         command,
-                        ] + command_args, **kwargs)
+                                  f"--bazelrc={self._bazel_rc.name}",
+                                  command,
+                                  ] + command_args, **kwargs)
 
     def setUp(self) -> None:
         self.assertTrue(os.environ.get("BUILD_WORKSPACE_DIRECTORY"),
-            "BUILD_WORKSPACE_DIRECTORY is not set")
+                        "BUILD_WORKSPACE_DIRECTORY is not set")
         os.chdir(os.environ["BUILD_WORKSPACE_DIRECTORY"])
         sys.stderr.write(f"BUILD_WORKSPACE_DIRECTORY={os.environ['BUILD_WORKSPACE_DIRECTORY']}\n")
 
@@ -136,6 +135,16 @@ class KleafIntegrationTest(unittest.TestCase):
 
         self._check_call("clean", [])
 
+    def restore_file_after_test(self, path: pathlib.Path | str):
+        with open(path) as file:
+            old_content = file.read()
+
+        def cleanup():
+            with open(path, "w") as new_file:
+                new_file.write(old_content)
+
+        self.addCleanup(cleanup)
+
     def _sha256(self, path: pathlib.Path | str) -> str:
         """Gets the hash for a file."""
         hash = hashlib.sha256()
@@ -148,14 +157,7 @@ class KleafIntegrationTest(unittest.TestCase):
 
     def _touch(self, path: pathlib.Path | str, append_text="\n") -> None:
         """Modifies a file so it (may) trigger a rebuild for certain targets."""
-        with open(path) as file:
-            old_content = file.read()
-
-        def cleanup():
-            with open(path, "w") as new_file:
-                new_file.write(old_content)
-
-        self.addCleanup(cleanup)
+        self.restore_file_after_test(path)
 
         with open(path, "a") as file:
             file.write(append_text)
@@ -295,6 +297,7 @@ class KleafIntegrationTest(unittest.TestCase):
             command="build",
             command_args=["//build/kernel/kleaf:empty_test"] + _FASTEST)
         self.assertFalse(default_out.exists())
+
 
 
 if __name__ == "__main__":
