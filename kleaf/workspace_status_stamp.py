d4570b183d7ea4ecfd11ac151233439c64f9ca1a
Yifan Hong <elsk@google.com>
2023-04-07 18:12:16 -0700

    kleaf: refactor workspace_status_stamp.py.
    Use OOP and split into multiple functions so it is easy
    to understand the data flow.
    
    This CL does not change any behavior.
    
    Bug: 277367919
    Test: TH
    Change-Id: I61a550de74cef3cbb63b31d9680fccf1dfe75a13

--- kleaf/workspace_status_stamp.py
+++ kleaf/workspace_status_stamp.py
@@ -16,9 +16,11 @@ import os
 import shutil
 import subprocess
 import sys
+from typing import Optional
 
 
-def call_setlocalversion(bin, srctree, *args):
+def call_setlocalversion(bin, srctree, *args) \
+        -> Optional[subprocess.Popen[str]]:
     """Call setlocalversion.
 
     Args:
@@ -37,7 +39,7 @@ def call_setlocalversion(bin, srctree, *args):
     return None
 
 
-def collect(popen_obj):
+def collect(popen_obj: subprocess.Popen) -> str:
     """Collect the result of a Popen object.
 
     Terminates the program if return code is non-zero.
@@ -53,72 +55,119 @@ def collect(popen_obj):
     return stdout.strip()
 
 
-def main():
-    kernel_dir = os.path.realpath(".source_date_epoch_dir")
-    has_kernel_dir = os.path.isdir(kernel_dir)
+class Stamp(object):
 
-    setlocalversion = os.path.join(kernel_dir, "scripts/setlocalversion")
-    if not has_kernel_dir or not os.access(setlocalversion, os.X_OK):
-        setlocalversion = None
+    def __init__(self):
+        self.init_for_dot_source_date_epoch_dir()
 
-    stable_scmversion_obj = None
-    if setlocalversion and has_kernel_dir:
-        stable_scmversion_obj = call_setlocalversion(setlocalversion,
-                                                     kernel_dir)
+    def init_for_dot_source_date_epoch_dir(self) -> None:
+        self.kernel_dir = os.path.realpath(".source_date_epoch_dir")
+        if not os.path.isdir(self.kernel_dir):
+            self.kernel_dir = None
 
-    ext_modules = []
-    stable_scmversion_extmod_objs = []
-    if setlocalversion:
-        ext_modules = []
+        self.find_setlocalversion()
+
+    def main(self) -> int:
+        kernel_dir_scmversion_obj = self.call_setlocalversion_kernel_dir()
+        ext_modules = self.get_ext_modules()
+        ext_mod_scmversion_objs = self.call_setlocalversion_ext_modules(
+            ext_modules)
+
+        source_date_epoch, source_date_epoch_obj = \
+            self.async_get_source_date_epoch_kernel_dir()
+
+        self.print_result(
+            kernel_dir_scmversion_obj=kernel_dir_scmversion_obj,
+            ext_modules=ext_modules,
+            ext_mod_scmversion_objs=ext_mod_scmversion_objs,
+            source_date_epoch=source_date_epoch,
+            source_date_epoch_obj=source_date_epoch_obj,
+        )
+        return 0
+
+    def find_setlocalversion(self) -> None:
+        self.setlocalversion = None
+        if self.kernel_dir:
+            candidate = os.path.join(self.kernel_dir,
+                                     "scripts/setlocalversion")
+            if os.access(candidate, os.X_OK):
+                self.setlocalversion = candidate
+
+    def call_setlocalversion_kernel_dir(self):
+        if not self.setlocalversion or not self.kernel_dir:
+            return None
+
+        return call_setlocalversion(self.setlocalversion, self.kernel_dir)
+
+    def get_ext_modules(self):
+        if not self.setlocalversion:
+            return []
         try:
-            ext_modules = subprocess.check_output(
-                """
+            cmd = """
                     source build/build_utils.sh
                     source build/_setup_env.sh
                     echo $EXT_MODULES
-                """,
-                shell=True,
-                text=True,
-                stderr=subprocess.PIPE,
-                executable="/bin/bash").split()
+                  """
+            return subprocess.check_output(cmd,
+                                           shell=True,
+                                           text=True,
+                                           stderr=subprocess.PIPE,
+                                           executable="/bin/bash").split()
         except subprocess.CalledProcessError as e:
-            msg = "WARNING: Unable to determine EXT_MODULES; scmversion for external modules may be incorrect. code={}, stderr={}\n".format(
-                e.returncode, e.stderr.strip())
+            msg = ("WARNING: Unable to determine EXT_MODULES; scmversion "
+                   "for external modules may be incorrect. "
+                   "code={}, stderr={}\n").format(e.returncode,
+                                                  e.stderr.strip())
             sys.stderr.write(msg)
-        stable_scmversion_extmod_objs = [
-            call_setlocalversion(setlocalversion, os.path.realpath(ext_mod))
-            for ext_mod in ext_modules
-        ]
+        return []
 
-    stable_source_date_epoch = os.environ.get("SOURCE_DATE_EPOCH")
-    stable_source_date_epoch_obj = None
-    if not stable_source_date_epoch and has_kernel_dir and shutil.which("git"):
-        stable_source_date_epoch_obj = subprocess.Popen(
-            ["git", "-C", kernel_dir, "log", "-1", "--pretty=%ct"],
-            text=True,
-            stdout=subprocess.PIPE)
-    else:
-        stable_source_date_epoch = 0
+    def call_setlocalversion_ext_modules(self, ext_modules):
+        if not self.setlocalversion:
+            return []
 
-    # Wait for subprocesses to finish, and print result.
+        ret = []
+        for ext_mod in ext_modules:
+            popen = call_setlocalversion(self.setlocalversion,
+                                         os.path.realpath(ext_mod))
+            ret.append(popen)
+        return ret
 
-    if stable_scmversion_obj:
-        print("STABLE_SCMVERSION", collect(stable_scmversion_obj))
+    def async_get_source_date_epoch_kernel_dir(self):
+        stable_source_date_epoch = os.environ.get("SOURCE_DATE_EPOCH")
+        stable_source_date_epoch_obj = None
+        if not stable_source_date_epoch and self.kernel_dir and \
+                shutil.which("git"):
+            stable_source_date_epoch_obj = subprocess.Popen(
+                ["git", "-C", self.kernel_dir, "log", "-1", "--pretty=%ct"],
+                text=True,
+                stdout=subprocess.PIPE)
+        else:
+            stable_source_date_epoch = 0
+        return stable_source_date_epoch, stable_source_date_epoch_obj
 
-    if stable_source_date_epoch_obj:
-        stable_source_date_epoch = collect(stable_source_date_epoch_obj)
-    print("STABLE_SOURCE_DATE_EPOCH", stable_source_date_epoch)
+    def print_result(
+        self,
+        kernel_dir_scmversion_obj,
+        ext_modules,
+        ext_mod_scmversion_objs,
+        source_date_epoch,
+        source_date_epoch_obj,
+    ) -> None:
+        if kernel_dir_scmversion_obj:
+            print("STABLE_SCMVERSION", collect(kernel_dir_scmversion_obj))
 
-    # If the list is empty, this prints "STABLE_SCMVERSION_EXT_MOD", and is
-    # filtered by Bazel.
-    print(
-        "STABLE_SCMVERSION_EXT_MOD",
-        " ".join("{}:{}".format(ext_mod, result) for ext_mod, result in zip(
-            ext_modules,
-            [collect(obj) for obj in stable_scmversion_extmod_objs])))
+        if source_date_epoch_obj:
+            source_date_epoch = collect(source_date_epoch_obj)
+        print("STABLE_SOURCE_DATE_EPOCH", source_date_epoch)
 
-    return 0
+        # If the list is empty, this prints "STABLE_SCMVERSION_EXT_MOD", and is
+        # filtered by Bazel.
+        print(
+            "STABLE_SCMVERSION_EXT_MOD",
+            " ".join("{}:{}".format(ext_mod, result) for ext_mod, result in
+                     zip(ext_modules,
+                         [collect(obj) for obj in ext_mod_scmversion_objs])))
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    sys.exit(Stamp().main())
