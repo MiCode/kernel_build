b12393c7947d859f615e24bfc4bd1ebf96126b13
jiahao <jiahao@xiaomi.com>
2023-05-06 19:59:57 +0800

    Kernel-Build: Xiaomi kernel changes for Xiaomi Pad 6
    The Patch based on QualComm release TAG: LA.UM.9.12.r1-13300-SMxx50.QSSI12.0-1
    
    Signed-off-by: jiahao <jiahao@xiaomi.com>
    Change-Id: Id17075972ca0cddbaa3f26fae341a35e17de5b21

--- build_test.sh
+++ build_test.sh
@@ -36,7 +36,7 @@ export BUILD_CONFIG
 
 test=all_tests.sh
 set -e
-source ${ROOT_DIR}/_setup_env.sh
+source ${ROOT_DIR}/envsetup.sh
 export OUT_DIR=$(readlink -m ${OUT_DIR:-${ROOT_DIR}/out/${BRANCH}})
 mkdir -p ${OUT_DIR}
 
--- static_analysis/checkpatch.sh
+++ static_analysis/checkpatch.sh
@@ -22,7 +22,7 @@ export STATIC_ANALYSIS_SRC_DIR=$(dirname $(readlink -f $0))
 
 ROOT_DIR="${STATIC_ANALYSIS_SRC_DIR}/../../"
 pushd ${ROOT_DIR}
-source ${STATIC_ANALYSIS_SRC_DIR}/../_setup_env.sh
+source ${STATIC_ANALYSIS_SRC_DIR}/../envsetup.sh
 export OUT_DIR=$(readlink -m ${OUT_DIR:-${ROOT_DIR}/out/${BRANCH}})
 export DIST_DIR=$(readlink -m ${DIST_DIR:-${OUT_DIR}/dist})
 mkdir -p ${DIST_DIR}
@@ -32,7 +32,7 @@ export KERNEL_DIR=$(readlink -m ${KERNEL_DIR})
 CHECKPATCH_PL_PATH="${KERNEL_DIR}/scripts/checkpatch.pl"
 GIT_SHA1="HEAD"
 PATCH_DIR="${OUT_DIR}/checkpatch/patches"
-IGNORELIST_FILE="${STATIC_ANALYSIS_SRC_DIR}/checkpatch_ignorelist"
+BLACKLIST_FILE="${STATIC_ANALYSIS_SRC_DIR}/checkpatch_blacklist"
 RESULTS_PATH=${DIST_DIR}/checkpatch.log
 RETURN_CODE=0
 
@@ -49,8 +49,8 @@ while [[ $# -gt 0 ]]; do
     GIT_SHA1="$2"
     shift
     ;;
-  --ignored_checks)
-    IGNORELIST_FILE="$2"
+  --blacklisted_checks)
+    BLACKLIST_FILE="$2"
     shift
     ;;
   --help)
@@ -59,7 +59,7 @@ while [[ $# -gt 0 ]]; do
     echo ""
     echo "Usage: $0"
     echo "  <--git_sha1 nnn> (Defaults to HEAD)"
-    echo "  <--ignored_checks path_to_file> (Defaults to checkpatch_ignorelist)"
+    echo "  <--blacklisted_checks path_to_file> (Defaults to checkpatch_blacklist)"
     echo "  <args for checkpatch.pl>"
     exit 0
     ;;
@@ -77,9 +77,9 @@ if [[ -d "${PATCH_DIR}" ]]; then
 fi
 mkdir -p "${PATCH_DIR}"
 
-# Update ignorelist.
-if [[ -f "${IGNORELIST_FILE}" ]]; then
-  IGNORED_ERRORS=$(grep -v '^#' ${IGNORELIST_FILE} | paste -s -d,)
+# Update blacklist.
+if [[ -f "${BLACKLIST_FILE}" ]]; then
+  IGNORED_ERRORS=$(grep -v '^#' ${BLACKLIST_FILE} | paste -s -d,)
   if [[ -n "${IGNORED_ERRORS}" ]]; then
     CHECKPATCH_ARGS+=(--ignore)
     CHECKPATCH_ARGS+=("${IGNORED_ERRORS}")
@@ -94,23 +94,17 @@ echo "Using --git_sha1: ${GIT_SHA1}"
 
 # Generate patch file from git.
 cd ${KERNEL_DIR}
-git format-patch --quiet -o "${PATCH_DIR}" "${GIT_SHA1}^1..${GIT_SHA1}" -- \
-  ':!android/abi*'
+git format-patch --quiet -o "${PATCH_DIR}" "${GIT_SHA1}^1..${GIT_SHA1}"
 PATCH_FILE="${PATCH_DIR}/*.patch"
-
-if ! `stat -t ${PATCH_FILE} >/dev/null 2>&1`; then
-  echo "Patch empty (probably due to suppressions). Skipping analysis."
-  exit 0
-fi
 
 # Delay exit on non-zero checkpatch.pl return code so we can finish logging.
 
 # Note, it's tricky to ignore this exit code completely and instead return only
-# based on the log values. For example, if the log is not empty, but contains
-# no ERRORS, how do we reliabliy distinguish WARNINGS that were not ignored (or
-# other conditions we want to ignore), from legitimate errors running the
+# based on the log values. For example, if the log is not empty, but contains no
+# ERRORS, how do we reliabliy distinguish WARNINGS that were not blacklisted
+# (or other conditions we want to ignore), from legitimate errors running the
 # script itself (e.g. bad flags)? checkpatch.pl will return 1 in both cases.
-# For now, include all known warnings in the ignorelist, and forward this code
+# For now, include all known warnings in the blacklist, and forward this code
 # unconditionally.
 
 set +e
@@ -124,7 +118,7 @@ if [[ $CHECKPATCH_RC -ne 0 ]]; then
   echoerr ""
   echoerr "Summary:"
   echoerr ""
-  { grep -r -h -E -A1 "^(ERROR|WARNING):" "${RESULTS_PATH}" 1>&2; } || true
+  { grep -r -h -E -A1 "^ERROR:" "${RESULTS_PATH}" 1>&2; } || true
   echoerr ""
   echoerr "See $(basename ${RESULTS_PATH}) for complete output."
 fi
