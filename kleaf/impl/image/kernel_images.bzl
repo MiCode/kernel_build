reocat/android_kernel_build

kleaf: Add support for specifying modules for recovery/charger
Currently, the same modules.load file is used for normal boot,
recovery/fastbootd and charger modes during first-stage init. This
means that during normal boot, the modules that are needed for
recovery and charger modes are loaded during first-stage init, which
is not required.

Add support for specifying a list of modules that are required for
booting into recovery or charger mode. This introduces two new
attributes to the kernel_images() macro called: modules_recovery_list
and modules_charger_list. Each attribute points to a list of modules
that are needed to boot into each mode respectively. This list can
be generated dynamically by concatenating the list of vendor_boot or
vendor_kernel_boot modules required for first stage init, along with
the list of modules needed to boot into each mode.

Defining these attributes also produces a file named
modules.load.[recovery/charger] in the initramfs, which init uses
instead of modules.load when booting into recovery or charger mode
respectively to load the first-stage init and recovery and charger
modules. In case of normal boot, init will use the modules.load
file, which will only contain the modules needed for first
stage init, and the recovery and charger modules will be loaded
from the vendor_dlkm partition during second-stage init.

Bug: 266752750
Change-Id: I11503b11683ef64cf0933b8641959ea5acd63ab1
Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>
(cherry picked from commit b9ba4b67e90b85d01eb2f9cebf62830a73574cd9)
[isaacmanjarres: Re-wrote some of the commit message/implemented it
as part of kleaf.]

kleaf/impl/image/kernel_images.bzl
@@ -46,6 +46,8 @@ def kernel_images(
         deps = None,
         boot_image_outs = None,
         modules_list = None,
+        modules_recovery_list = None,
+        modules_charger_list = None,
         modules_blocklist = None,
         modules_options = None,
         vendor_ramdisk_binaries = None,
@@ -120,6 +122,8 @@ def kernel_images(
           - For `initramfs`:
             - The file specified by `MODULES_LIST`
             - The file specified by `MODULES_BLOCKLIST`, if `MODULES_BLOCKLIST` is set
+            - The file containing the list of modules needed for booting into recovery.
+            - The file containing the list of modules needed for booting into charger mode.
           - For `vendor_dlkm` image:
             - The file specified by `VENDOR_DLKM_MODULES_LIST`
             - The file specified by `VENDOR_DLKM_MODULES_BLOCKLIST`, if set
@@ -214,6 +218,10 @@ def kernel_images(
         modules_list: A file containing list of modules to use for `vendor_boot.modules.load`.
 
           This corresponds to `MODULES_LIST` in `build.config` for `build.sh`.
+        modules_recovery_list: A file containing a list of modules to load when booting into
+          recovery.
+        modules_charger_list: A file containing a list of modules to load when booting into
+          charger mode.
         modules_blocklist: A file containing a list of modules which are
           blocked from being loaded.
 
@@ -376,12 +384,23 @@ def kernel_images(
     if build_vendor_kernel_boot and "vendor_kernel_boot.img" not in boot_image_outs:
         boot_image_outs.append("vendor_kernel_boot.img")
 
+    vendor_boot_name = None
+    if build_vendor_boot:
+        vendor_boot_name = "vendor_boot"
+    elif build_vendor_kernel_boot:
+        vendor_boot_name = "vendor_kernel_boot"
+
     vendor_boot_modules_load = None
+    vendor_boot_modules_load_recovery = None
+    vendor_boot_modules_load_charger = None
     if build_initramfs:
-        if build_vendor_boot:
-            vendor_boot_modules_load = "{}_initramfs/vendor_boot.modules.load".format(name)
-        elif build_vendor_kernel_boot:
-            vendor_boot_modules_load = "{}_initramfs/vendor_kernel_boot.modules.load".format(name)
+        vendor_boot_modules_load = "{}_initramfs/{}.modules.load".format(name, vendor_boot_name)
+
+        if modules_recovery_list:
+            vendor_boot_modules_load_recovery = "{}_initramfs/{}.modules.load.recovery".format(name, vendor_boot_name)
+
+        if modules_charger_list:
+            vendor_boot_modules_load_charger = "{}_initramfs/{}.modules.load.recovery".format(name, vendor_boot_name)
 
         if ramdisk_compression_args and ramdisk_compression != "lz4":
             fail(
@@ -395,7 +414,11 @@ def kernel_images(
             kernel_modules_install = kernel_modules_install,
             deps = deps,
             vendor_boot_modules_load = vendor_boot_modules_load,
+            vendor_boot_modules_load_recovery = vendor_boot_modules_load_recovery,
+            vendor_boot_modules_load_charger = vendor_boot_modules_load_charger,
             modules_list = modules_list,
+            modules_recovery_list = modules_recovery_list,
+            modules_charger_list = modules_charger_list,
             modules_blocklist = modules_blocklist,
             modules_options = modules_options,
             ramdisk_compression = ramdisk_compression,
@@ -447,12 +470,6 @@ def kernel_images(
         all_rules.append(":{}_vendor_dlkm_image".format(name))
 
     if build_any_boot_image:
-        if build_vendor_kernel_boot:
-            vendor_boot_name = "vendor_kernel_boot"
-        elif build_vendor_boot:
-            vendor_boot_name = "vendor_boot"
-        else:
-            vendor_boot_name = None
         boot_images(
             name = "{}_boot_images".format(name),
             kernel_build = kernel_build,

MiCode/kernel_build

as part of kleaf. Also added tests for the new attributes.]
(cherry picked from commit d08605f4c795e05d8569e975affd2736e430a555)
[isaacmanjarres: Resolved trivial merge conflicts when backporting
from main.]

kleaf/impl/image/kernel_images.bzl
@@ -42,6 +42,8 @@ def kernel_images(
         deps = None,
         boot_image_outs = None,
         modules_list = None,
+        modules_recovery_list = None,
+        modules_charger_list = None,
         modules_blocklist = None,
         modules_options = None,
         vendor_ramdisk_binaries = None,
@@ -86,6 +88,8 @@ def kernel_images(
           - For `initramfs`:
             - The file specified by `MODULES_LIST`
             - The file specified by `MODULES_BLOCKLIST`, if `MODULES_BLOCKLIST` is set
+            - The file containing the list of modules needed for booting into recovery.
+            - The file containing the list of modules needed for booting into charger mode.
           - For `vendor_dlkm` image:
             - The file specified by `VENDOR_DLKM_MODULES_LIST`
             - The file specified by `VENDOR_DLKM_MODULES_BLOCKLIST`, if set
@@ -180,6 +184,10 @@ def kernel_images(
         modules_list: A file containing list of modules to use for `vendor_boot.modules.load`.
 
           This corresponds to `MODULES_LIST` in `build.config` for `build.sh`.
+        modules_recovery_list: A file containing a list of modules to load when booting into
+          recovery.
+        modules_charger_list: A file containing a list of modules to load when booting into
+          charger mode.
         modules_blocklist: A file containing a list of modules which are
           blocked from being loaded.
 
@@ -339,12 +347,23 @@ def kernel_images(
     if build_vendor_kernel_boot and "vendor_kernel_boot.img" not in boot_image_outs:
         boot_image_outs.append("vendor_kernel_boot.img")
 
+    vendor_boot_name = None
+    if build_vendor_boot:
+        vendor_boot_name = "vendor_boot"
+    elif build_vendor_kernel_boot:
+        vendor_boot_name = "vendor_kernel_boot"
+
     vendor_boot_modules_load = None
+    vendor_boot_modules_load_recovery = None
+    vendor_boot_modules_load_charger = None
     if build_initramfs:
-        if build_vendor_boot:
-            vendor_boot_modules_load = "{}_initramfs/vendor_boot.modules.load".format(name)
-        elif build_vendor_kernel_boot:
-            vendor_boot_modules_load = "{}_initramfs/vendor_kernel_boot.modules.load".format(name)
+        vendor_boot_modules_load = "{}_initramfs/{}.modules.load".format(name, vendor_boot_name)
+
+        if modules_recovery_list:
+            vendor_boot_modules_load_recovery = "{}_initramfs/{}.modules.load.recovery".format(name, vendor_boot_name)
+
+        if modules_charger_list:
+            vendor_boot_modules_load_charger = "{}_initramfs/{}.modules.load.charger".format(name, vendor_boot_name)
 
         if ramdisk_compression_args and ramdisk_compression != "lz4":
             fail(
@@ -358,7 +377,11 @@ def kernel_images(
             kernel_modules_install = kernel_modules_install,
             deps = deps,
             vendor_boot_modules_load = vendor_boot_modules_load,
+            vendor_boot_modules_load_recovery = vendor_boot_modules_load_recovery,
+            vendor_boot_modules_load_charger = vendor_boot_modules_load_charger,
             modules_list = modules_list,
+            modules_recovery_list = modules_recovery_list,
+            modules_charger_list = modules_charger_list,
             modules_blocklist = modules_blocklist,
             modules_options = modules_options,
             ramdisk_compression = ramdisk_compression,
@@ -410,12 +433,6 @@ def kernel_images(
         all_rules.append(":{}_vendor_dlkm_image".format(name))
 
     if build_any_boot_image:
-        if build_vendor_kernel_boot:
-            vendor_boot_name = "vendor_kernel_boot"
-        elif build_vendor_boot:
-            vendor_boot_name = "vendor_boot"
-        else:
-            vendor_boot_name = None
         boot_images(
             name = "{}_boot_images".format(name),
             kernel_build = kernel_build,